b861f1df7f2d4531a3294ab3d2dcb0ca6eb41a4a
Bhramara M Chickamuniswamy <bmachavalahalli@deloitte.nl>
2017-02-11 23:55:41 +0100

    Charging contract changes

--- server/api/charging.js
+++ server/api/charging.js
@@ -9,17 +9,19 @@ const apiHelper = require('./helper');
 
 module.exports = (app) => {
 
-  
+
   app.post('/charging/deploy', (req, res) => {
+    console.log("Request is received");
+    console.log("Request body is: " + req.body.userAddress);
+    console.log(JSON.stringify(req.body));
     if (!req.body.userAddress)
-      return apiHelper.formatError(res, 403, 'User address is required');
+      return apiHelper.formatError(res, {code: 403, message:'UserAddress is required'});
     if (!req.body.homeAddress)
-      return apiHelper.formatError(res, {code: 403, message:'Validator API URL is required'});
+      return apiHelper.formatError(res, {code: 403, message:'HomeAddress is required'});
     if (!req.body.carAddress)
-      return apiHelper.formatError(res, {code: 403, message:'RiskProvider API URL API URL is required'} );
+      return apiHelper.formatError(res, {code: 403, message:'CarAddress is required'} );
     if (!req.body.sellPrice)
-      return apiHelper.formatError(res, {code: 403, message:'CheckListProvider API URL is required'} );
-    if (!req.body.startingPointenergy)
+      return apiHelper.formatError(res, {code: 403, message:'SellPrice is required'} );
     blockchainService.deployContract(req.body.userAddress, req.body.homeAddress, req.body.carAddress, req.body.sellPrice, req.body.startingPointenergy)
       .then(contractAddress => res.status(200).send(contractAddress))
       .catch(err => apiHelper.formatError(res, err));
@@ -27,25 +29,22 @@ module.exports = (app) => {
 
   app.post('/charging/:contractAddress/chargeStarted', (req, res) => {
     if (!req.body.userAddress)
-      return apiHelper.formatError(res, {code: 403, message:'Contract URL is required'});
-    if (!req.body.contractAddress)
-      return apiHelper.formatError(res, {code: 403, message:'Contract URL is required'});
+      return apiHelper.formatError(res, {code: 403, message:'UserAddress is required'});
     if (!req.body.amountToKeep)
-      return apiHelper.formatError(res, {code: 403, message:'Validator API URL is required'});
+      return apiHelper.formatError(res, {code: 403, message:'AmountToKeep is required'});
 
-    blockchainService.chargeStarted(req.body.userAddress, req.body.contractAddress, req.body.amountToKeep)
+    blockchainService.chargeStarted(req.body.userAddress, req.params.contractAddress, req.body.amountToKeep)
       .then(contractAddress => res.status(200).send(contractAddress))
       .catch(err => apiHelper.formatError(res, err));
   });
 
   app.post('/charging/:contractAddress/chargeCompleted', (req, res) => {
     if (!req.body.userAddress)
-      return apiHelper.formatError(res, {code: 403, message:'Contract URL is required'});
+      return apiHelper.formatError(res, {code: 403, message:'UserAddress is required'});
     if (!req.body.contractAddress)
-      return apiHelper.formatError(res, {code: 403, message:'Contract URL is required'});
+      return apiHelper.formatError(res, {code: 403, message:'ContractAddress is required'});
     if (!req.body.endMeterReading)
-      return apiHelper.formatError(res, {code: 403, message:'Validator API URL is required'});
-   
+      return apiHelper.formatError(res, {code: 403, message:'EndMeterReading is required'});
 
     blockchainService.chargeCompleted(req.body.userAddress, req.body.contractAddress, req.body.endMeterReading)
       .then(contractAddress => res.status(200).send(contractAddress))
@@ -54,7 +53,7 @@ module.exports = (app) => {
 
   app.get('/charging/:contractAddress/status', (req, res) => {
     blockchainService.getStatus(req.params.contractAddress)
-      .then(prosumer => res.status(200).send(prosumer))
+      .then(contractAddress => res.status(200).send(contractAddress))
       .catch(err => apiHelper.formatError(res, 500, err));
   });
 

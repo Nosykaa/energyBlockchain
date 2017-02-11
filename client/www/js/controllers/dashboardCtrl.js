fbf131764aaeb685b375e5f86728f33821a51726
Desmond Peace <Desmond@Desmonds-MacBook-Pro-2.local>
2017-02-11 15:30:22 +0100

    Finished Dashboard Home and added all routes

--- client/www/js/controllers/dashboardCtrl.js
+++ client/www/js/controllers/dashboardCtrl.js
@@ -1,5 +1,45 @@
 controllers
-.controller('DashboardCtrl', ['$scope', '$rootScope', '$http', 'Place', 'Profile',
-                              function ($scope, $rootScope, $http) {
-    console.log('whatever mann');
+.controller('DashboardCtrl', ['$scope', '$rootScope', '$http', '$timeout', '$state',
+                              function ($scope, $rootScope, $http, $timeout, $state) {
+    /**
+    * Init variable
+    */
+    $scope.isLoading = false;
+
+    $scope.dispache = false;
+
+    $scope.listOfCars = function () {
+      $scope.dispache = true;
+      $timeout(function () {
+          $state.go('listofcars');
+      }, 1000);
+    };
+
+    $scope.moneyEarned = function () {
+      $scope.dispache = true;
+      $timeout(function () {
+          $state.go('moneyearned');
+      }, 1000);
+    };
+
+    $scope.energytradeoff = function () {
+      $scope.dispache = true;
+      $timeout(function () {
+          $state.go('energytradeoff');
+      }, 1000);
+    };
+
+    $scope.currentEnergyPrices = function () {
+      $scope.dispache = true;
+      $timeout(function () {
+          $state.go('currentenergyprices');
+      }, 1000);
+    };
+
+    $scope.myHomeSettings = function () {
+      $scope.dispache = true;
+      $timeout(function () {
+          $state.go('myhomesettings');
+      }, 1000);
+    };
 }]);

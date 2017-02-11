47c4ed8604a2ef9b9335b6d4ddc75f759ffc6c9e
Bachir Arif <dzer45@gmail.com>
2017-02-11 23:12:43 +0100

    Fix Bux Balance front

--- server/api/eth.js
+++ server/api/eth.js
@@ -4,11 +4,8 @@ const error = require('../services/error');
 module.exports = app => {
 
   app.get('/balance/:userAddress/', (req, res) => {
-
     balanceService.getBalance(req.params.userAddress)
-      .then(data => res.status(200).json({
-        amount: data.amount,
-      }))
+      .then(data => res.status(200).json(data))
       .catch(err => error.display(res, err));
   });
 

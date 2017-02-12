bfc5d818f389ab3ac2a3a181771f22ac919b075e
Bachir Arif <dzer45@gmail.com>
2017-02-12 10:30:32 +0100

    Pre finish

--- client/www/js/controllers/rootCtrl.js
+++ client/www/js/controllers/rootCtrl.js
@@ -11,21 +11,8 @@ controllers
             }
         };
 
-        $scope.elapsed = false;
+      $scope.elapsed = false;
       $timeout(function(){$scope.elapsed = true}, 10000);
-
-
-      GeoLocalisation.getPosition().then(function (position) {
-
-
-
-      }, function () {
-          $ionicPopup.alert({
-              title: 'Problème',
-              template: 'La géolocalisation n\'est pas fonctionnelle !'
-
-          });
-      });
 
       $ionicModal.fromTemplateUrl('templates/modal-feedback.html', {
           scope: $scope,
@@ -36,24 +23,7 @@ controllers
 
       $scope.feedback = function(places){
 
-        if(places){
-          GeoLocalisation.getPosition().then(function(position){
-            Place.freePlace(position.coords.longitude, position.coords.latitude).success(function (data) {
-                  console.log('freeplace ok')
-            })
-          });
-        }
-        if($rootScope.user){
-                $rootScope.user.score += 6;
-        }
-
-        $ionicPopup.alert({
-            title: 'Merci d\'avoir signalé une place vide ;)',
-            template: 'Vous avez gagné 6 points !'
-        }).then(function(){
-          $state.go('home')
-        })
+        $state.go('cockpit')
         $rootScope.modalFeedback.hide();
-
       }
 }]);
